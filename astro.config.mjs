aece68dffee85472b3e075ab74dbebad865d0f91
Harutyun-Mkhitaryan <162716179+Harutyun-Mkhitaryan@users.noreply.github.com>
2026-05-20 10:24:44 +0400

    fix: correctly restore tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://Harutyun-Mkhitaryan.github.io', 
-  // ВАЖНО: здесь теперь новое имя репозитория!
-  base: '/Mariam-Art', 
+  site: 'https://Harutyun-Mkhitaryan.github.io',
+  base: '/Mariam-Art',
+  integrations: [tailwind()]
 });
